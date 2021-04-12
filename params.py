2d4bd2429058674a7322e3f710e147ed7a50d306
mrodsam <marta171294@gmail.com>
2021-04-12 19:27:05 +0200

    Parámetros para SIFT

---
+++ params.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 23 10:35:28 2019
+
+@author: martarodriguezsampayo
+"""
+#Número de octavas
+nOCTAVES = 4
+
+#Intervalos por octava
+INTVLS = 3
+
+#Sigma inicial
+SIGMA = 1.6
+
+#Sigma para el suavizado inicial de la imagen de entrada
+INIT_SIGMA = 0.5
+
+#Sigma pre-blur??
+PREBLUR_SIGMA = 1
+
+#Umbral de contraste
+CONTR_THR = 0.03
+
+#Umbral del ratio de curvaturas principales
+CURV_THR = 10
+
+#Número de intervalos del histograma
+NUM_BINS = 36
